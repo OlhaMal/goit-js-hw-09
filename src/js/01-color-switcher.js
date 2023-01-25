006e037d05e7b24409dba142ce06ab62246c1615
OlhaMal <117120298+OlhaMal@users.noreply.github.com>
2023-01-25 20:51:10 +0200

    done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const bodyEL = document.querySelector('body')
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+stopBtn.disabled = true;
+let timerId = null;
+
+startBtn.addEventListener('click', () => {
+    startBtn.disabled = true;
+    stopBtn.disabled = false
+
+    timerId = setInterval(() => {
+        bodyEL.style.backgroundColor = getRandomHexColor()
+    }, 700)
+})
+
+stopBtn.addEventListener('click', () => {
+    stopBtn.disabled= true;
+    startBtn.disabled = false;
+    clearInterval(timerId);
+})
